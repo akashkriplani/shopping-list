99f8f418a4cdf995d986c2cc085ed6f28dd207e4
Akash <aakash23kriplani@gmail.com>
2022-02-24 13:02:03 +0530

    Delete ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -58,6 +58,10 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onDeleteIngredient(index: number): void {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   onCancel(): void {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
