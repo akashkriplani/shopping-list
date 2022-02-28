d28c9847efeead150e4329c1cdbc331e7bdad0d3
Akash <aakash23kriplani@gmail.com>
2022-02-28 10:12:23 +0530

    Redirect on successful authentication

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
 
@@ -13,7 +14,7 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -41,6 +42,7 @@ export class AuthComponent implements OnInit {
       (response) => {
         this.isLoading = false;
         console.log(response);
+        this.router.navigate(['/recipes']);
       },
       (errorMessage) => {
         this.isLoading = false;
