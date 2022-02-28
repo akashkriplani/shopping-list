cd9988210e478aec1a2c1732ed99b609f2e6281f
Akash <aakash23kriplani@gmail.com>
2022-02-28 20:20:49 +0530

    Add lazy loading for shopping list and auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [
-    BrowserModule,
-    AuthModule,
-    HttpClientModule,
-    AppRoutingModule,
-    ShoppingListModule,
-    SharedModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
